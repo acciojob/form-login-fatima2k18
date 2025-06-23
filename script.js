4e43a923901555a7b798baa27045bc74a7db2ce4
Fatima Ejaz <92662988+fatima2k18@users.noreply.github.com>
2025-06-23 07:21:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
     //Write your code here
+	  event.preventDefault(); // Prevent form reload
+
+      const form = document.forms[0]; // Get the first (and only) form
+      let fname = form.fname.value.trim();
+      let lname = form.lname.value.trim();
+
+      // Optional edge case: if both fields are empty
+      if (!fname && !lname) {
+        alert("Please enter your full name.");
+        return;
+      }
+
+      alert(`${fname} ${lname}`); // Show full name
 
 }
